dc6a57dcf1f296dceaadf5af703ccf65fb15cb7e
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-09 23:48:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,27 +1,19 @@
 def my_hash
-  my_hash= { "name"=>"Gabriela","age"=>"22"
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {"name" => "Alexis", "age" => 26, "location" => "LA"}
 end
 
 
 def shipping_manifest
-  the_manifest= {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-
+  #set a variable called `shipping_manifest`, equal to a hash
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-        shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -30,4 +22,3 @@ def adding
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 end
-
