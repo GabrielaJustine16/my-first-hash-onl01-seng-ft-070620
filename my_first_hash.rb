72ae4072e907889aab3d5cfa3adc151daa91179f
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-09 23:46:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  your_hash= { "name"=>"Gabriela","age"=>"22"
+  my_hash= { "name"=>"Gabriela","age"=>"22"
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
